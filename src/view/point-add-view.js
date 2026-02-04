2fabbb51c9ce82bd8ca907fa6431db1f7d9d3d1a
Foxsiny <mitiashkinata8@gmail.com>
2026-02-05 00:10:03 +0300

    feat: implement AbstractStatefulView and internal logic for PointAddView

--- src/view/point-add-view.js
+++ src/view/point-add-view.js
@@ -1,4 +1,4 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import { POINT_TYPES } from '../const.js';
 
 // Вспомогательная функция для создания одного пункта списка типов
@@ -19,8 +19,8 @@ const createEventTypeItemTemplate = (type, currentType) => `
 `;
 
 // Вспомогательная функция для генерации одной опции
-const createOfferSelectorTemplate = (offer) => {
-  const {id, title, price, isChecked} = offer;
+const createOfferSelectorTemplate = (offer, isChecked) => {
+  const {id, title, price} = offer;
 
   return `
     <div class="event__offer-selector">
@@ -29,6 +29,7 @@ const createOfferSelectorTemplate = (offer) => {
         id="event-offer-${id}-1"
         type="checkbox"
         name="event-offer-${id}"
+        data-offer-id="${id}"  // <--- Добавь это для связи со стейтом
         ${isChecked ? 'checked' : ''}
       >
       <label class="event__offer-label" for="event-offer-${id}-1">
@@ -41,7 +42,7 @@ const createOfferSelectorTemplate = (offer) => {
 };
 
 // Функция для создания ВСЕЙ секции офферов
-const createOffersSectionTemplate = (offers) => {
+const createOffersSectionTemplate = (offers, selectedOfferIds) => {
   if (!offers || offers.length === 0) {
     return '';
   }
@@ -50,13 +51,15 @@ const createOffersSectionTemplate = (offers) => {
     <section class="event__section  event__section--offers">
       <h3 class="event__section-title  event__section-title--offers">Offers</h3>
       <div class="event__available-offers">
-        ${offers.map((offer) => createOfferSelectorTemplate(offer)).join('')}
+        ${offers.map((offer) => createOfferSelectorTemplate(offer, selectedOfferIds.includes(offer.id))).join('')}
       </div>
     </section>`;
 };
 
-const createPointAddTemplate = (offers) => {
-  const currentType = 'flight'; // Пока статично, как в вашей разметке
+const createPointAddTemplate = (state, destinations, offers) => {
+  const {type, basePrice, destination: destinationId, offers: selectedOfferIds} = state;
+  const pointDestination = destinations.find((dest) => dest.id === destinationId);
+  const offersByType = offers.find((offer) => offer.type === type)?.offers || [];
 
   return `
   <li class="trip-events__item">
@@ -65,7 +68,7 @@ const createPointAddTemplate = (offers) => {
       <div class="event__type-wrapper">
         <label class="event__type  event__type-btn" for="event-type-toggle-1">
           <span class="visually-hidden">Choose event type</span>
-          <img class="event__type-icon" width="17" height="17" src="img/icons/${currentType}.png" alt="Event type icon"/>
+          <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon"/>
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox"/>
 
@@ -73,7 +76,7 @@ const createPointAddTemplate = (offers) => {
           <fieldset class="event__type-group">
             <legend class="visually-hidden">Event type</legend>
 
-            ${POINT_TYPES.map((type) => createEventTypeItemTemplate(type, currentType)).join('')}
+            ${POINT_TYPES.map((pointType) => createEventTypeItemTemplate(pointType, type)).join('')}
 
           </fieldset>
         </div>
@@ -81,14 +84,12 @@ const createPointAddTemplate = (offers) => {
 
       <div class="event__field-group  event__field-group--destination">
         <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
+          ${type}
         </label>
         <input class="event__input  event__input--destination" id="event-destination-1" type="text"
-               name="event-destination" value="Chamonix" list="destination-list-1"/>
+               name="event-destination" value="${pointDestination ? pointDestination.name : ''}" list="destination-list-1"/>
         <datalist id="destination-list-1">
-          <option value="Amsterdam"></option>
-          <option value="Geneva"></option>
-          <option value="Chamonix"></option>
+          ${destinations.map((dest) => `<option value="${dest.name}"></option>`).join('')}
         </datalist>
       </div>
 
@@ -108,7 +109,7 @@ const createPointAddTemplate = (offers) => {
           &euro;
         </label>
         <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price"
-               value=""/>
+               value="${basePrice}"/>
       </div>
 
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
@@ -116,50 +117,92 @@ const createPointAddTemplate = (offers) => {
 
     </header>
     <section class="event__details">
-
-          ${createOffersSectionTemplate(offers)}
-
-      <section class="event__section  event__section--destination">
-        <h3 class="event__section-title  event__section-title--destination">Destination</h3>
-        <p class="event__destination-description">Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.</p>
-      </section>
+      ${createOffersSectionTemplate(offersByType, selectedOfferIds)}
+      ${pointDestination ? `
+        <section class="event__section  event__section--destination">
+          <h3 class="event__section-title  event__section-title--destination">Destination</h3>
+          <p class="event__destination-description">${pointDestination.description}</p>
+        </section>` : ''}
     </section>
   </form>
-  </li>
-  `;
+  </li>`;
 };
 
-export default class PointAddView extends AbstractView {
+export default class PointAddView extends AbstractStatefulView {
+  #destinations = null;
   #offers = null;
   #handleFormSubmit = null;
   #handleCancelClick = null;
 
-  constructor({ offers, onFormSubmit, onCancelClick }) {
+  constructor({ destinations, offers, onFormSubmit, onCancelClick }) {
     super();
+
+    // Начальное состояние для НОВОЙ точки
+    this._setState({
+      type: 'flight',
+      destination: destinations[0].id, // По умолчанию первый город
+      basePrice: 0,
+      offers: []
+    });
+
+    this.#destinations = destinations;
     this.#offers = offers;
     this.#handleFormSubmit = onFormSubmit;
     this.#handleCancelClick = onCancelClick;
 
-    // Навешиваем слушатель на кнопку Save (отправка формы)
-    this.element.querySelector('form')
-      .addEventListener('submit', this.#formSubmitHandler);
-
-    // Навешиваем слушатель на кнопку Cancel (тип reset)
-    this.element.querySelector('.event__reset-btn')
-      .addEventListener('click', this.#cancelClickHandler);
+    this._restoreHandlers();
   }
 
   get template() {
-    return createPointAddTemplate(this.#offers);
+    return createPointAddTemplate(this._state, this.#destinations, this.#offers);
   }
+
+  _restoreHandlers() {
+    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+    this.element.querySelector('.event__reset-btn').addEventListener('click', this.#cancelClickHandler);
+    // noinspection DuplicatedCode
+    this.element.querySelector('.event__type-group').addEventListener('change', this.#typeChangeHandler);
+    this.element.querySelector('.event__input--destination').addEventListener('change', this.#destinationChangeHandler);
+    this.element.querySelector('.event__available-offers')?.addEventListener('change', this.#offerChangeHandler);
+    this.element.querySelector('.event__input--price').addEventListener('input', this.#priceChangeHandler);
+  }
+
+  #typeChangeHandler = (evt) => {
+    this.updateElement({ type: evt.target.value, offers: [] });
+  };
+
+  #destinationChangeHandler = (evt) => {
+    const selected = this.#destinations.find((dest) => dest.name === evt.target.value);
+    if (!selected) {
+      return;
+    }
+    this.updateElement({ destination: selected.id });
+  };
+
+  #offerChangeHandler = (evt) => {
+    const offerId = evt.target.dataset.offerId;
+    const currentOffers = [...this._state.offers];
+    const index = currentOffers.indexOf(offerId);
+    if (index === -1) {
+      currentOffers.push(offerId);
+    } else {
+      currentOffers.splice(index, 1);
+    }
+    this._setState({ offers: currentOffers });
+  };
+
+  #priceChangeHandler = (evt) => {
+    this._setState({ basePrice: Number(evt.target.value) });
+  };
+
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this.#handleFormSubmit();
+    this.#handleFormSubmit?.(this._state);
   };
 
   #cancelClickHandler = (evt) => {
     evt.preventDefault();
-    this.#handleCancelClick();
+    this.#handleCancelClick?.();
   };
 }
