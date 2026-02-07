cf9c592ff5ae6cfaed572c4b967ff8b38ed9eca8
Foxsiny <mitiashkinata8@gmail.com>
2026-02-07 10:53:57 +0300

    feat: implement dynamic duration calculation in PointView and final UI polish

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,4 +1,6 @@
 import dayjs from 'dayjs';
+import duration from 'dayjs/plugin/duration';
+dayjs.extend(duration);
 
 const DATE_FORMAT = 'MMM D';
 const TIME_FORMAT = 'HH:mm';
@@ -6,6 +8,21 @@ const TIME_FORMAT = 'HH:mm';
 const humanizePointDate = (date) => date ? dayjs(date).format(DATE_FORMAT) : '';
 const humanizePointTime = (date) => date ? dayjs(date).format(TIME_FORMAT) : '';
 
+const getPointDuration = (dateFrom, dateTo) => {
+  const diff = dayjs(dateTo).diff(dayjs(dateFrom)); // Разница в миллисекундах
+  const pointDuration = dayjs.duration(diff);
+
+  if (pointDuration.asDays() >= 1) {
+    // Если больше суток: 01D 02H 30M
+    return pointDuration.format('DD[D] HH[H] mm[M]');
+  } else if (pointDuration.asHours() >= 1) {
+    // Если больше часа: 02H 30M
+    return pointDuration.format('HH[H] mm[M]');
+  } else {
+    // Если меньше часа: 30M
+    return pointDuration.format('mm[M]');
+  }
+};
 
 const isPointFuture = (date) => date && dayjs().isBefore(date, 'D');
 const isPointPast = (date) => date && dayjs().isAfter(date, 'D');
@@ -36,5 +53,6 @@ export {
   isPointPresent,
   sortPointDay,
   sortPointTime,
-  sortPointPrice
+  sortPointPrice,
+  getPointDuration
 };
